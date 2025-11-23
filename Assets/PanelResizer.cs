28567e8b09c3a18a250aa89763b088681c1d74e4
DeckBuilder <devnull@example.com>
2025-11-23 14:24:13 -0500

    Initial commit

---
+++ Assets/PanelResizer.cs
@@ -0,0 +1,24 @@
+// 11/23/2025 AI-Tag
+// This was created with the help of Assistant, a Unity Artificial Intelligence product.
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class PanelResizer : MonoBehaviour
+{
+    private RectTransform panelRectTransform;
+
+    void Start()
+    {
+        panelRectTransform = GetComponent<RectTransform>();
+        Debug.Log("rectTransform: " + panelRectTransform);
+        if (panelRectTransform != null)
+        {
+            panelRectTransform.anchorMin = new Vector2(0, 0);
+            panelRectTransform.anchorMax = new Vector2(1, 1);
+            panelRectTransform.offsetMin = Vector2.zero;
+            panelRectTransform.offsetMax = Vector2.zero;
+        }
+    }
+}
